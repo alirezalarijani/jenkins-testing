8d0fbfa6a56609013ec805aa9b2cf0682f46152b
unknown <alireza.larijani@utexas.edu>
2014-10-30 23:05:54 -0700

    nose.bat updated for code coverage report

--- nose.bat
+++ nose.bat
@@ -1,1 +1,2 @@
-c:\python27\scripts\nosetests -v --with-coverage ExperimentTest.py
+c:\python27\scripts\nosetests --with-xunit --all-modules --traverse-namespace --with-coverage --cover-inclusive  Experiment.py
+c:\python27\python -m coverage xml --include Experiment.py
